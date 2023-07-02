f11d9b87321a4b10421aa37171cfbb0f536af9ba
Hanzala Sohrab <sorb.hanzala@gmail.com>
2023-07-02 13:39:45 +0530

    Display word meaning in an HTML dialog

--- synonymous.js
+++ synonymous.js
@@ -1,4 +1,4 @@
-function getSelectionText() {
+const getSelectionText = async () => {
   var text = "";
   if (window.getSelection) {
     text = window.getSelection().toString();
@@ -8,19 +8,41 @@ function getSelectionText() {
   return text;
 }
 
+const displaySynonym = async (synonym) => {
+  // Create a dialog element
+  const dialog = document.createElement('dialog');
+  dialog.textContent = synonym; // Set the content of the dialog
+
+  // Append the dialog to the document
+  document.body.appendChild(dialog);
+
+  // Close the dialog when the user clicks anywhere
+  document.addEventListener('click', () => {
+    if (dialog.open) {
+      dialog.close();
+    }
+  });
+
+  // Open the dialog
+  dialog.showModal();
+}
+
 async function getMeaning() {
-  const text = getSelectionText().toLowerCase();
+  const text = (await getSelectionText())?.toLowerCase();
+
   const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${text}`;
-  // console.log('URL = ', url);
+
   const response = await fetch(url, { method: "GET", mode: "no-cors" }).then(data => data.text()).then(data => JSON.parse(data));
-  const meaning = response?.[0].meanings?.[0].definitions?.[0]?.definition;
-  // console.log('DATA = ', response);
+
+  const meaning = response?.[0]?.meanings?.[0].definitions?.[0]?.definition;
+
   console.log(`Meaning of ${text} is ${meaning}`);
+
+  await displaySynonym(meaning);
 }
 
 document.addEventListener("keydown", async (e) => {
-  console.log(`Key pressed = ${e.key}`);
-  if (e.ctrlKey && e.key.toLowerCase() === 'm') {
+  if (e.ctrlKey && e.altKey && e.key.toLowerCase() === 'm') {
     await getMeaning();
   }
 })
